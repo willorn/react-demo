a9e806c32de5f9d853b23ee4026b7dc777d4d97e
Tianyi <willorn@qq.com>
2026-06-28 16:38:22 +0800

    add .gitignore and bind challenge controls to React state
    - ignore OS/editor files, logs, node_modules, and build output
    - fix React CDN import by using global React object
    - wire up purple checkbox and text color select to component state

--- 1-challenge-circle/main.js
+++ 1-challenge-circle/main.js
@@ -9,18 +9,30 @@
   transform: `rotate(0deg)`,
 */
 
+const { useState } = React;
+
 function MyApp() {
+  const [isPurple, setIsPurple] = useState(false); // 是否紫色, 默认不是
+  const [textColor, setTextColor] = useState(""); // 文本颜色, 默认白
+  const [circleSize, setCircleSize] = useState(150);
+  const [circleRotate, setCircleRotate] = useState(0);
+  const circleClass = isPurple ? "purple" : "";
+
   return (
     <main>
       <label>
-        Purple
-        <input type="checkbox" />
+        紫色
+        <input
+          type="checkbox"
+          checked={isPurple}
+          onChange={() => setIsPurple(!isPurple)}
+        />
       </label>
 
       <label>
-        text color
-        <select>
-          <option value="" selected>
+        文本颜色
+        <select value={textColor} onChange={(e) => setTextColor(e.target.value)}>
+          <option value="text-white" selected>
             White
           </option>
           <option value="text-black">Black</option>
@@ -30,14 +42,24 @@ function MyApp() {
 
       <label>
         Circle Size
-        <input type="number" value="150" />
+        <input type="number" value={circleSize} onChange={(e) => setCircleSize(e.target.value)} />
       </label>
 
       <label>
         Circle Rotate
-        <input type="number" value="0" />
-      </label>
-      <div className="circle">Hi!</div>
+        <input type="number" value={circleRotate} onChange={(e) => setCircleRotate(e.target.value)} />
+        </label>
+      <div
+        className={`circle ${circleClass} ${textColor}`}
+        style={{
+          height: `${circleSize}px`,
+          width: `${circleSize}px`,
+          lineHeight: `${circleSize}px`,
+          transform: `rotate(${circleRotate}deg)`,
+        }}
+      >
+        Hi!
+      </div>
     </main>
   );
 }
